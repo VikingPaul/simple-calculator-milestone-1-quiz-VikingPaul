a96c0cc8a003e9e3326fda6bb561a7e04e42ecc8
VikingPaul <wip321@yahoo.com>
2016-08-15 20:24:36 -0500

    groovy, the maths work.

---
+++ SimpleCalculator/Expression.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SimpleCalculator
+{
+    public class Expression
+    {
+        string[] array = new string[0];
+        public Expression(string expression)
+        {
+            string pattern = "(\\d*)\\s?(\\+?\\-?\\/?\\*?)\\s?(\\d*)";
+            array = Regex.Split(expression, pattern);
+            array[0] = "true";
+            try
+            {
+                int first = int.Parse(array[1]);
+            }
+            catch (FormatException)
+            {
+                
+                array[0] = "false";
+            }
+            try
+            {
+                int second = int.Parse(array[3]);
+            }
+            catch (FormatException)
+            {
+                array[0] = "false";
+            }
+        }
+        public string[] getArray()
+        {
+            return array;
+        }
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,10 +10,24 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            Stack GetSetLast = new Stack();
             bool playProgram = true;
             int numOfExpressions = 0;
             while(playProgram)
             {
+                string whiteSpace = "";
+                if (numOfExpressions > 99)
+                {
+                    whiteSpace = "     ";
+                }
+                else if (numOfExpressions > 9)
+                {
+                    whiteSpace = "    ";
+                }
+                else
+                {
+                    whiteSpace = "   ";
+                }
                 Console.Write("[" + numOfExpressions + "]>");
                 string newCommand = Console.ReadLine().ToLower();
                 numOfExpressions++;
@@ -26,10 +40,24 @@ namespace SimpleCalculator
                         playProgram = false;
                         break;
                     case "last":
+                        Console.WriteLine(whiteSpace + GetSetLast.LastAnswer);
                         break;
                     case "lastq":
+                        Console.WriteLine(whiteSpace + GetSetLast.LastQuestion);
                         break;
                     default:
+                        string[] trueExpression = new Expression(newCommand).getArray();
+                        if (trueExpression[0] == "false")
+                        {
+                            Console.WriteLine(whiteSpace + "Invalid");
+                        }
+                        else
+                        {
+                            int ans = new Maths(int.Parse(trueExpression[1]), trueExpression[2], int.Parse(trueExpression[3])).Answer();
+                            GetSetLast.LastAnswer = ans.ToString();
+                            GetSetLast.LastQuestion = newCommand;
+                            Console.WriteLine(whiteSpace + "=" + ans.ToString());
+                        }
                         break;
                 }
                     
